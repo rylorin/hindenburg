1496161a3d91bb3abb43539922c208adf3a52e88
Ronan-Yann Lorin <ryl@free.fr>
2024-02-25 22:38:17 +0100

    Minor improvements

--- src/SmtpClient.ts
+++ src/SmtpClient.ts
@@ -13,17 +13,21 @@ export class SmtpClient {
         user: process.env.RELAY_USERNAME,
         pass: process.env.RELAY_PASSWORD,
       },
+      logger: true,
     });
   }
 
-  public async forwardEmail(email: Record<string, any>) {
-    const info = await this.transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: "rylorin@gmail.com", // list of receivers
-      subject: email.subject, // Subject line
-      text: email.text, // plain text body
-      html: email.textAsHtml, // html body
-    });
+  public async forwardEmail(emailIn: Record<string, any>) {
+    const emailOut = {
+      from:
+        emailIn.headers.get("from").text || '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: process.env.RCPT_TO || emailIn.to || "<foo@bar.com>", // list of receivers
+      subject: emailIn.subject, // Subject line
+      text: emailIn.text, // plain text body
+      html: emailIn.html, // html body
+    };
+    console.log(emailOut);
+    const info = await this.transporter.sendMail(emailOut);
 
     console.error("Message sent: %s", info.messageId);
   }
